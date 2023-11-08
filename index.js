2d7f45018c79772fe676845adcc4bbe299857f25
monaemhossain <monaemhs@gmail.com>
2023-11-08 17:35:04 +0300

    added jwt

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}))
 app.use(express.json());
-
+app.use(cookieParser());
 
 
 
@@ -34,6 +39,20 @@ async function run() {
 
         const JobCollections = client.db('jobDB').collection('job');
         const applicationCollections = client.db('applicationDB').collection('application');
+
+        // auth api
+        app.post('/jwt', async(req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, `${process.env.ACCESS_TOKEN_SECRET}`, {expiresIn: '1h'})
+            console.log(user);
+            res
+            .cookie('token', token, {
+                httpOnly: true,
+                secure: false,
+            })
+            .send({success: true})
+        })
+
         // Jobs Collection
         // write jobs
         app.post('/job', async (req, res) => {
@@ -57,10 +76,10 @@ async function run() {
         // update job
         app.put('/job/:id', async (req, res) => {
             const id = req.params.id;
-            const filter = {_id: new ObjectId(id)}
-            const options = {upsert:true}
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true }
             const updateJob = req.body;
-            
+
             const job = {
                 $set: {
                     bannerPhoto: updateJob.bannerPhoto,
@@ -80,26 +99,38 @@ async function run() {
             const result = await JobCollections.updateOne(filter, job, options);
             res.send(result);
         })
-        // delete cart item
+        // delete job item
         app.delete('/job/:id', async (req, res) => {
             const id = req.params.id
             const query = { _id: new ObjectId(id) }
             const result = await JobCollections.deleteOne(query)
             res.send(result)
         })
-
+        // search job
+        app.get('/search/:key', async (req, res) => {
+            const key = req.params.key.toLowerCase();
+            const regex = new RegExp(key, 'i')
+            const result = await JobCollections.find(
+                {$or: [
+                    { jobTitle: regex },
+                    { jobType: regex },
+                ]}
+            ).toArray()
+            res.send(result);
+        });
 
 
 
 
         // Job Application api
-        app.post('/application', async(req,res) => {
+        app.post('/application', async (req, res) => {
             newApplication = req.body;
+            console.log(req.cookies);
             const result = await applicationCollections.insertOne(newApplication);
             res.send(result);
         })
         // Read Applications
-        app.get('/application', async(req, res) => {
+        app.get('/application', async (req, res) => {
             const cursor = applicationCollections.find();
             const result = await cursor.toArray();
             res.send(result);
@@ -112,7 +143,18 @@ async function run() {
             res.send(result);
         })
 
-
+        app.get('/search-application/:key', async (req, res) => {
+            const key = req.params.key.toLowerCase();
+            const regex = new RegExp(key, 'i')
+            const result = await applicationCollections.find(
+                {$or: [
+                    { jobTitle: regex },
+                    { jobType: regex },
+                ]}
+            ).toArray()
+            console.log(result);
+            res.send(result);
+        });
 
 
 
