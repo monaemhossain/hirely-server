a80621d64fd078d309bc5fdabc593e5e1aca9e3b
monaemhossain <monaemhs@gmail.com>
2023-11-08 19:12:35 +0300

    change cors origin link

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['https://hire-ly.web.app'],
     credentials: true
 }))
 app.use(express.json());
