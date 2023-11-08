33f0a4a5a78e348c63db63a6982eb5880575ba2a
monaemhossain <monaemhs@gmail.com>
2023-11-08 04:38:03 +0300

    added functionality to update job

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ async function run() {
         // await client.connect();
 
         const JobCollections = client.db('jobDB').collection('job');
+        const applicationCollections = client.db('applicationDB').collection('application');
         // Jobs Collection
         // write jobs
         app.post('/job', async (req, res) => {
@@ -51,6 +52,53 @@ async function run() {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
             const result = await JobCollections.findOne(query);
+            res.send(result);
+        })
+        // update job
+        app.put('/job/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = {_id: new ObjectId(id)}
+            const options = {upsert:true}
+            const updateJob = req.body;
+            
+            const job = {
+                $set: {
+                    bannerPhoto: updateJob.bannerPhoto,
+                    userName: updateJob.userName,
+                    jobTitle: updateJob.jobTitle,
+                    userEmail: updateJob.userEmail,
+                    jobType: updateJob.jobType,
+                    jobCategory: updateJob.jobCategory,
+                    postingDate: updateJob.postingDate,
+                    deadLine: updateJob.deadLine,
+                    jobDescription: updateJob.jobDescription,
+                    priceRageMin: updateJob.priceRageMin,
+                    priceRageMax: updateJob.priceRageMax,
+                    applicantsNumber: updateJob.applicantsNumber,
+                }
+            }
+            const result = await JobCollections.updateOne(filter, job, options);
+            res.send(result);
+        })
+
+
+        // Job Application api
+        app.post('/application', async(req,res) => {
+            newApplication = req.body;
+            const result = await applicationCollections.insertOne(newApplication);
+            res.send(result);
+        })
+        // Read Applications
+        app.get('/application', async(req, res) => {
+            const cursor = applicationCollections.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+        // single application by id
+        app.get('/application/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await applicationCollections.findOne(query);
             res.send(result);
         })
 
